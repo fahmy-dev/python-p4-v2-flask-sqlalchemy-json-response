64b0b9032a5ebe6c5b0bfcf91d01080214c49072
fahmy-dev <faahmy@proton.me>
2025-03-29 00:33:34 +0300

    commit

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ from models import db, Pet
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
@@ -17,16 +17,48 @@ db.init_app(app)
 @app.route('/')
 def index():
     return make_response(
-        '<h1>Welcome to the pet directory!</h1>',
+        {'message': 'Welcome to the pet directory!'},
         200
     )
 
 
-@app.route('/demo_json')
-def demo_json():
-    pet_json = '{"id": 1, "name" : "Fido", "species" : "Dog"}'
-    return make_response(pet_json, 200)
+@app.route('/pets/<int:id>')
+def get_pet_by_id(id):
+    pet = Pet.query.filter_by(id=id).first()
 
+    if not pet:
+        return make_response(
+            {"error": "Pet not found"}, 404
+        )
+
+    pet_dict = {
+        'id': pet.id, 
+        'name' : pet.name, 
+        'species' : pet.species
+    }
+    return make_response(pet_dict, 200)
+
+@app.route('/species/<string:species>')
+def get_pets_by_species(species):
+    pets = Pet.query.filter_by(species=species).all()
+
+    if not pets:
+        return make_response(
+            {'error': f'No pets found of species {species}'}, 404
+        )
+
+    pets_list = []
+    for pet in pets:
+        pet_dict = {
+            'id': pet.id,
+            'name': pet.name,
+            'species': pet.species
+        }
+        pets_list.append(pet_dict)
+
+    return make_response(
+        {'pets': pets_list}, 200
+    )
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
